8d174327916e2de4c7ec6d6fb7509e042f2cf84b
Ramon-Arias25 <56901379+Ramon-Arias25@users.noreply.github.com>
2021-01-20 21:49:36 -0300

    with build

---
+++ build/precache-manifest.e38b1ad7c1ba773743bbab139ccfb125.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0406d409f64bb8ec720a61d2b3684a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f262ebc21611ea6e4538",
+    "url": "/static/css/2.c3c19069.chunk.css"
+  },
+  {
+    "revision": "a83622fecf13134cc6c7",
+    "url": "/static/css/main.f7feeb47.chunk.css"
+  },
+  {
+    "revision": "f262ebc21611ea6e4538",
+    "url": "/static/js/2.271ae679.chunk.js"
+  },
+  {
+    "revision": "a83622fecf13134cc6c7",
+    "url": "/static/js/main.4fb6e649.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
